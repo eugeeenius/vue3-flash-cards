b0d23f6ecc24dd4fb47f4f41a34071d59e040ff2
Eugene Glushchuk <e.glushuk@idaproject.com>
2021-08-20 14:11:52 +0300

    feat(decks-page): added list transition

--- src/views/Deck.vue
+++ src/views/Deck.vue
@@ -2,11 +2,27 @@
     <div v-if="isDeckListEmpty">Decks List is empty</div>
 
     <template v-else>
-        <DeckCard
-            v-for="deck in decks"
-            :key="deck.id"
-            :deck="deck"
-        />
+        <div class="v-input v-input--dirty v-input--variant-filled v-theme--light v-input--density-default">
+            <div class="v-input__control">
+                <div class="v-input__overlay"></div>
+
+                <div class="v-input__field">
+                    <input type="text"
+                           class="pa-0 px-6"
+                           @change="onChange">
+                </div>
+
+                <div class="v-input__outline"></div>
+            </div>
+        </div>
+
+        <transition-group name="bottom-appear">
+            <DeckCard
+                v-for="deck in filteredDecks"
+                :key="deck.id"
+                :deck="deck"
+            />
+        </transition-group>
     </template>
 </template>
 
@@ -14,9 +30,11 @@
 import {
     computed,
     defineComponent,
+    ref,
 } from 'vue';
-
 import { useStore } from 'vuex';
+
+import { Deck } from '@/models/Deck';
 
 import DeckCard from '@/components/DeckCard.vue';
 
@@ -25,10 +43,30 @@ export default defineComponent({
 
     setup() {
         const store = useStore();
+        const decks = computed(() => store.getters['decks/getDecksList']);
+
+        const filteredDecks = ref<Deck[]>(decks.value);
+
+        const filterDecks = (e: Event) => {
+            filteredDecks.value = [];
+
+            setTimeout(() => {
+                const input = e.target as HTMLInputElement;
+                const value = input.value.trim();
+
+                if (!value) {
+                    filteredDecks.value = decks.value;
+                    return;
+                }
+
+                filteredDecks.value = decks.value.filter((deck: Deck) => deck.title.toLowerCase().includes(value));
+            }, 400);
+        };
 
         return {
             isDeckListEmpty: computed(() => store.getters['decks/isDecksListEmpty']),
-            decks: computed(() => store.getters['decks/getDecksList']),
+            onChange: filterDecks,
+            filteredDecks,
         };
     },
 });
